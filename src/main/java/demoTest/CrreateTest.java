dbbe7b7908f5f93051454deef9f0d86b4b1d1c5f
GANESH <GANESH@Ganesh>
2022-11-19 12:42:47 +0530

    Commit and push

---
+++ src/main/java/demoTest/CrreateTest.java
@@ -0,0 +1,10 @@
+package demoTest;
+
+public class CrreateTest {
+
+	public static void main(String[] args) {
+		
+			System.out.println("This is demo framework");
+			System.out.println("hie");
+			}
+}
